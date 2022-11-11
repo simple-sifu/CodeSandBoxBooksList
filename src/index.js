63459061b28d3464dbdd1546bac301b10aacd978
Tommy Han <36891099+simple-sifu@users.noreply.github.com>
2022-11-11 16:04:19 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  //const [vm, copyVmToComponentState] = useState([]);
+
+  React.useEffect(() => {
+    //async function load() {
+    // const booksPresenter = new BooksPresenter();
+    // const generatedVm = await booksPresenter.load();
+    // copyVmToComponentState(generatedVm);
+    //}
+    // load();
+  }, []);
+
+  return (
+    <div>
+      {/* {vm.map((book, i) => {
+        return <div key={i}>{book.visibleName}</div>;
+      })} */}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
